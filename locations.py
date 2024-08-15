17897a8cab1691dee8dd59a34cf1e2a66ba0ae67
Ross Dunn SE2020 <ross.dunn.2020@uni.strath.ac.uk>
2024-08-15 19:12:29 +0100

    Locations py script

---
+++ locations.py
@@ -0,0 +1,7 @@
+# List of locations in Glasgow
+
+glasgow_east = '55.8537, -4.2055' # Note this seems to return 0 results within a 1km radius (can alter distance)
+glasgow_west = '55.8753, -4.2917'
+glasgow_north = '55.8895, -4.2413'
+glasgow_south = '55.8367, -4.2733'
+glasgow_centre = '55.8609, -4.2514'
---
+++ map_connect.py
@@ -0,0 +1,41 @@
+import requests
+from locations import glasgow_east,glasgow_west,glasgow_north,glasgow_south,glasgow_centre
+from dotenv import load_dotenv
+import os
+
+# Api key load
+load_dotenv()
+api_key = os.getenv('api_key')
+
+search_radius = '1000' # this is in meters - so a 1km radius across the given area
+venue_type = 'night_club' # this is in place of pubs 
+
+
+# Function to locate different bars within each given location within glasgow
+def locate_pubs(area_coords):
+    search = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+    
+    search_parameters = {
+        'location': area_coords,
+        'radius': search_radius,
+        'type': venue_type,
+        'key': api_key,
+    }
+
+    response = requests.get(search, params=search_parameters)
+
+    if response.status_code == 200:
+        query_data = response.json()
+        print("Glasgow pubs located!: ")
+        for bar in query_data['results']:
+            print (bar)
+            print('\n')
+       
+    else:
+        print("Error in query!", response.status_code, response.text)
+
+
+locate_pubs(glasgow_centre)
+        
+
+
