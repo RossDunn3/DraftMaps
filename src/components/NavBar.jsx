5486d6a050b463f361f999339815c07e47956938
RossDunn3 <ross1dunn@gmail.com>
2024-08-29 13:36:04 +0100

    Home page changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,63 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import Home from '../pages/Home';
+import Beerlogo from '/src/assets/beer-transparent.png'
 
 const NavBar = () => {
   return (
-    <>
-    <nav>
-  <ul className="flex bg-slate-500 text-white h-10 items-center p-6 text-lg">
-    <li className='mr-6'>
-      <Link to="/">DraftMaps</Link>
-    </li>
-      <li className='mr-6'>
-        <Link to="/map">Map </Link>
-      </li>
-  </ul>
-</nav>
-    <Outlet />
-    </>
+    <nav className="">
+    <div className="h-30 flex items-center justify-between p-5">
+      
+      <div className="flex text-3xl font-mono italic items-center bg-orange-500 rounded-md p-2 text-white shadow-xl">
+        DraftMaps
+      </div>
+
+      <ul className="flex space-x-4 text-xl ">
+        <li>
+          <Link to="/home" className="hover:underline">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="hover:underline">
+            Map
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" className="hover:underline">
+            Contact
+          </Link>
+        </li>
+        <div class="w-0.5 h-8 bg-gray-300"></div>
+        <li>
+        <Link to="/community" className="hover:underline">
+            Community
+          </Link>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+        <Link to="/" className=" bg-orange-500 rounded-md p-2 text-white shadow-xl">
+            Sign in
+          </Link>
+        </li>
+        <li>
+        <Link to="/" className="">
+
+        </Link>
+        </li>
+      </ul>
+      
+    </div>
+
+    
+  </nav>
   );
 };
 
 export default NavBar;
+
+
+
+     
